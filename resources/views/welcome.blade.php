57ee864bc5a57f32206729e9f4d460316cb87d5b
Kickinbrain <kickinbrain@gmail.com>
2021-01-19 00:18:16 +0300

    init project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CSS for BackEnd dev</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.2/tailwind.min.css">
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body class="flex items-center justify-center">
+    <div class="card flex">
+
+        <div class="card-left">
+            <a href="#" class="card-skill">PHP</a>
+            <img src="https://laracasts.s3.amazonaws.com/series/thumbnails/10-techniques-for-cleaner-code.png?v=8" width="98" height="98">
+            <span class="card-diff">Intermedia dificulty</span>
+        </div>
+
+        <div class="card-right">
+            <h3 class="card-title"><a href="#">Testing jargonton</a></h3>
+            <p class="card-exerpt">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Accusamus aperiam beatae eligendi minus molestiae nobis provident
+                quas vel vitae voluptas.
+            </p>
+            <div class="card-meta">
+                <div>
+                    <img src="https://image.flaticon.com/icons/svg/864/864685.svg"  width="15" height="15">
+                    5 lessons
+                </div>
+                <div>
+                    <img src="https://image.flaticon.com/icons/svg/2088/2088617.svg"  width="15" height="15">
+                    2:40 min
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
